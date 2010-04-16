60706b2c2c8f92d3c3f0846a65c9e01a34a064e0
sbrandhof <sbrandhof@dev.java.net>
2010-04-16 08:25:35 +0000

    sonar plugin: remove copyright (with the agreement of the current owner) + add LGPL header to all classes

--- src/main/java/hudson/plugins/sonar/template/SimpleTemplate.java
+++ src/main/java/hudson/plugins/sonar/template/SimpleTemplate.java
@@ -1,8 +1,4 @@
 /*
- * Sonar, entreprise quality control tool.
- * Copyright (C) 2007-2008 Hortis-GRC SA
- * mailto:be_agile HAT hortis DOT ch
- *
  * Sonar is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
