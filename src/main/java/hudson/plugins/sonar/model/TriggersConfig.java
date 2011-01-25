9d884c2b8980074bc9e39dadb3b80527aea4d0b4
rseguy <romain.seguy@gmail.com>
2011-01-25 09:32:16 +0100

    [FIXED SONARPLUGINS-570]

--- src/main/java/hudson/plugins/sonar/model/TriggersConfig.java
+++ src/main/java/hudson/plugins/sonar/model/TriggersConfig.java
@@ -18,8 +18,9 @@ package hudson.plugins.sonar.model;
 import hudson.model.AbstractBuild;
 import hudson.model.Cause;
 import hudson.model.CauseAction;
+import hudson.model.Hudson;
+import hudson.model.Job;
 import hudson.model.Result;
-import hudson.plugins.sonar.Messages;
 import hudson.triggers.SCMTrigger;
 import hudson.triggers.TimerTrigger;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -41,6 +42,8 @@ public class TriggersConfig implements Serializable {
    */
   private boolean userBuilds;
 
+  private boolean descendantOfUserBuilds;
+
   private boolean snapshotDependencyBuilds;
 
   private boolean skipIfBuildFails;
@@ -49,10 +52,11 @@ public class TriggersConfig implements Serializable {
   }
 
   @DataBoundConstructor
-  public TriggersConfig(boolean scmBuilds, boolean timerBuilds, boolean userBuilds, boolean snapshotDependencyBuilds, boolean skipIfBuildFails) {
+  public TriggersConfig(boolean scmBuilds, boolean timerBuilds, boolean userBuilds, boolean descendantOfUserBuilds, boolean snapshotDependencyBuilds, boolean skipIfBuildFails) {
     this.scmBuilds = scmBuilds;
     this.timerBuilds = timerBuilds;
     this.userBuilds = userBuilds;
+    this.descendantOfUserBuilds = descendantOfUserBuilds;
     this.snapshotDependencyBuilds = snapshotDependencyBuilds;
     this.skipIfBuildFails = skipIfBuildFails;
   }
@@ -81,6 +85,14 @@ public class TriggersConfig implements Serializable {
     this.userBuilds = userBuilds;
   }
 
+  public boolean isDescendantOfUserBuilds() {
+    return descendantOfUserBuilds;
+  }
+
+  public void setDescendantOfUserBuilds(boolean descendantOfUserBuilds) {
+    this.descendantOfUserBuilds = descendantOfUserBuilds;
+  }
+
   public boolean isSnapshotDependencyBuilds() {
     return snapshotDependencyBuilds;
   }
@@ -97,21 +109,24 @@ public class TriggersConfig implements Serializable {
     this.skipIfBuildFails = skipIfBuildFails;
   }
 
-  public String isSkipSonar(AbstractBuild<?, ?> build) {
-    if (isSkipIfBuildFails() && build.getResult().isWorseThan(Result.UNSTABLE)) {
-      return Messages.SonarPublisher_BadBuildStatus(build.getResult().toString());
-    } else if (build.getResult().isWorseThan(Result.FAILURE)) {
-      return Messages.SonarPublisher_BadBuildStatus(build.getResult().toString());
-    } else if (!isScmBuilds() && isTrigger(build, SCMTrigger.SCMTriggerCause.class)) {
-      return Messages.SonarPublisher_SCMBuild();
-    } else if (!isTimerBuilds() && isTrigger(build, TimerTrigger.TimerTriggerCause.class)) {
-      return Messages.SonarPublisher_TimerBuild();
-    } else if (!isUserBuilds() && isTrigger(build, Cause.UserCause.class)) {
-      return Messages.SonarPublisher_UserBuild();
-    } else if (!isSnapshotDependencyBuilds() && isTrigger(build, Cause.UpstreamCause.class)) {
-      return Messages.SonarPublisher_SnapshotDepBuild();
+  /**
+   * Returns {@code true} if the Sonar analysis has to be performed based on the
+   * triggers configuration, {@code false} otherwise.
+   */
+  public boolean doPerformSonar(AbstractBuild<?, ?> build) {
+    if (build.getResult().isWorseThan(Result.FAILURE) ||
+            isSkipIfBuildFails() && build.getResult().isWorseThan(Result.UNSTABLE)) { // Skip analysis on build failure
+      return false;
     }
-    return null;
+    else if (isScmBuilds() && isTrigger(build, SCMTrigger.SCMTriggerCause.class) || // Poll SCM
+            isTimerBuilds() && isTrigger(build, TimerTrigger.TimerTriggerCause.class) || // Build periodically
+            isUserBuilds() && isTrigger(build, Cause.UserCause.class) || // Manually started by user
+            isDescendantOfUserBuilds() && isAscendantTrigger(build, Cause.UserCause.class) || // Descendant of a build manually started by user
+            isSnapshotDependencyBuilds() && isTrigger(build, Cause.UpstreamCause.class)) { // Build whenever a SNAPSHOT dependency is built
+      return true;
+    }
+
+    return false;
   }
 
   /**
@@ -124,6 +139,7 @@ public class TriggersConfig implements Serializable {
   private static boolean isTrigger(AbstractBuild<?, ?> build, Class<? extends hudson.model.Cause> trigger) {
     CauseAction buildCause = build.getAction(CauseAction.class);
     List<Cause> buildCauses = buildCause.getCauses();
+
     for (Cause cause : buildCauses) {
       if (trigger.isInstance(cause)) {
         return true;
@@ -131,4 +147,44 @@ public class TriggersConfig implements Serializable {
     }
     return false;
   }
+
+  /**
+   * Returns true if the specified build has been triggered, or one of its ascendant,
+   * by the specified {@code trigger}.
+   *
+   * @param build Build to check
+   * @param trigger Trigger for the specified build
+   */
+  private static boolean isAscendantTrigger(AbstractBuild<?, ?> build, Class<? extends hudson.model.Cause> trigger) {
+    try {
+      AbstractBuild currentBuild = build;
+      List<Cause> currentCauses = currentBuild.getAction(CauseAction.class).getCauses();
+
+      while(currentCauses.size() == 1) {
+        Cause cause = currentCauses.get(0);
+        if(cause instanceof Cause.UpstreamCause) {
+          // let's move to the upstream build
+          String currentProject = ((Cause.UpstreamCause) cause).getUpstreamProject();
+          currentBuild = (AbstractBuild) ((Job) Hudson.getInstance().getItem(currentProject)).getBuildByNumber(((Cause.UpstreamCause) cause).getUpstreamBuild());
+          currentCauses = currentBuild.getAction(CauseAction.class).getCauses();
+        }
+        else if(trigger.isInstance(cause)) {
+          // the trigger is the expected one
+          return true;
+        }
+        else {
+          // another cause, let's make all this fail then
+          break;
+        }
+      }
+    }
+    catch(Exception e) {
+      // it would be far too complicated to handle all the potential cases that
+      // may occur about upstream builds, so let's handle that through a basic
+      // exception
+    }
+
+    return false;
+  }
+
 }
