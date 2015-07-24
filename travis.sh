2a42e45f9fc6da40c0fdecc894283e522f80adff
David Gageot <david@gageot.net>
2015-07-24 14:51:01 +0200

    Use travis-utils v15

--- travis.sh
+++ travis.sh
@@ -3,23 +3,23 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v14/install.sh | bash
-  source /tmp/travis-utils/env.sh
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v15 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
 }
 
 mvn verify -B -e -V
 
-if [ "${RUN_ITS}" == "true" ]
-then
+if [ "${RUN_ITS}" == "true" ]; then
   installTravisTools
 
   if [ "${SQ_VERSION}" == "DEV" ]
   then
-    travis_build_green "SonarSource/sonarqube" "master"
+    build_snapshot "SonarSource/sonarqube"
   fi
 
-  travis_start_xvfb
+  start_xvfb
 
-  cd its  
-  mvn -Djenkins.runtimeVersion=$JENKINS_VERSION -Dsonar.runtimeVersion=$SQ_VERSION -Dmaven.test.redirectTestOutputToFile=false verify
+  cd its
+  mvn -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify
 fi
