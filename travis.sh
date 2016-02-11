4053413e2081d15def677fdcab3dc6c09116209a
Julien HENRY <julien.henry@sonarsource.com>
2016-02-11 14:38:49 +0100

    Update travis configuration

--- travis.sh
+++ travis.sh
@@ -8,36 +8,55 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
-if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
-then
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-  then
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
+
+case "$TARGET" in
+
+CI)
+  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    strongEcho "Build and analyze commit in master"
+    # this commit is master must be built and analyzed (with upload of report)
+    mvn verify -B -e -V
+
+    # integration of jacoco report is quite memory-consuming
+    export MAVEN_OPTS="-Xmx1536m -Xms128m"
+    git fetch --unshallow
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test sonar:sonar -B -e -V \
+       -Dsonar.host.url=$SONAR_HOST_URL \
+       -Dsonar.login=$SONAR_TOKEN
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
     # For security reasons environment variables are not available on the pull requests
     # coming from outside repositories
     # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
+    strongEcho "Build and analyze pull request"
+    mvn verify -B -e -V
 
-      # PR analysis
-      # Accessing Dory with SSL requires Java 8
-      source $HOME/.jdk_switcher_rc
-      jdk_switcher use oraclejdk8    
-      mvn verify sonar:sonar -B -e -V \
+    export MAVEN_OPTS="-Xmx1G -Xms128m"
+    mvn verify sonar:sonar -B -e -V \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
         -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_LOGIN \
-        -Dsonar.password=$SONAR_PASSWD
-    fi
-  fi
-else
-  # Regular CI
-  mvn verify -B -e -V
-fi
+        -Dsonar.login=$SONAR_TOKEN
 
-if [ "${RUN_ITS}" == "true" ]; then
+  else
+    strongEcho 'Build, no analysis'
+    # Build branch, without any analysis
+
+    # No need for Maven goal "install" as the generated JAR file does not need to be installed
+    # in Maven local repository
+    mvn verify -B -e -V
+  fi
+  ;;
+
+IT)
+  mvn package -B -e -V -DskipTests
   installTravisTools
 
   if [ "${SQ_VERSION}" == "DEV" ]
@@ -48,5 +67,12 @@ if [ "${RUN_ITS}" == "true" ]; then
   start_xvfb
 
   cd its
-  mvn -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify
-fi
+  mvn -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify 
+  ;;
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
