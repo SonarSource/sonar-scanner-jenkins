14eb14ebc95a7b0080421d666da0c1c7ae79b2cc
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-04-25 11:07:24 +0200

    SONARJNKNS-247 Migration of global configuration not saved

--- src/main/java/hudson/plugins/sonar/SonarGlobalConfiguration.java
+++ src/main/java/hudson/plugins/sonar/SonarGlobalConfiguration.java
@@ -55,12 +55,10 @@ public class SonarGlobalConfiguration extends GlobalConfiguration {
    * @return all configured {@link hudson.plugins.sonar.SonarInstallation}
    */
   public SonarInstallation[] getInstallations() {
-    migrate();
     return installations;
   }
 
   public boolean isBuildWrapperEnabled() {
-    migrate();
     return buildWrapperEnabled;
   }
 
@@ -79,7 +77,7 @@ public class SonarGlobalConfiguration extends GlobalConfiguration {
    * It is thread safe and will refuse to migrate if a SonarQube installation already exists in this class.
    * Migration will only be attempted once. 
    */
-  private void migrate() {
+  public void migrate() {
     if (migrated) {
       return;
     }
@@ -101,8 +99,10 @@ public class SonarGlobalConfiguration extends GlobalConfiguration {
 
         publisher.deleteGlobalConfiguration();
       }
+      
       migrated = true;
     }
+    save();
   }
 
   @Override
