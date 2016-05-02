5bc9d49df19320897f4fb3f00a609e44f49cff34
tomverin <thomas.verin@sonarsource.com>
2016-05-02 14:27:49 +0200

    Debug cix.sh

--- cix.sh
+++ cix.sh
@@ -6,12 +6,15 @@ JENKINS_VERSION=1.580.3
  
 #deploy the version built by travis
 CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
+echo "CURRENT_VERSION=$CURRENT_VERSION"
 RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
+echo "RELEASE_VERSION=$RELEASE_VERSION"
 NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
-echo $NEW_VERSION
+echo "NEW_VERSION=$NEW_VERSION"
 mkdir -p target
 cd target
 curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/jenkins-ci/plugins/sonar/$NEW_VERSION/sonar-$NEW_VERSION.hpi
+echo "Downloaded sonar-$NEW_VERSION.hpi"
 mv sonar-$NEW_VERSION.hpi sonar.hpi
 cd ..
 
