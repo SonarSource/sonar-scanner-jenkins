356cd16acdb63bb388b95782881335c0d18a0b87
Julien HENRY <julien.henry@sonarsource.com>
2016-09-13 16:33:13 +0200

    Stabilize ITs

--- cix.sh
+++ cix.sh
@@ -11,6 +11,7 @@ RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
 echo "RELEASE_VERSION=$RELEASE_VERSION"
 NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
 echo "NEW_VERSION=$NEW_VERSION"
+rm -rf target
 mkdir -p target
 cd target
 curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/jenkins-ci/plugins/sonar/$NEW_VERSION/sonar-$NEW_VERSION.hpi
@@ -19,4 +20,4 @@ mv sonar-$NEW_VERSION.hpi sonar.hpi
 cd ..
 
 cd its
-mvn -B -e -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify 
+mvn -B -e -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false clean verify 
