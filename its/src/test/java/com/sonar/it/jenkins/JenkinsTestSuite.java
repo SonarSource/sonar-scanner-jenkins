e5a124d11f91ad87f63bf481adb59e7d1d0f337d
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-09-15 11:39:49 +0200

    Remove pipeline tests from common suite

--- its/src/test/java/com/sonar/it/jenkins/JenkinsTestSuite.java
+++ its/src/test/java/com/sonar/it/jenkins/JenkinsTestSuite.java
@@ -28,7 +28,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({JenkinsTest.class, JenkinsWithoutMaven.class, JenkinsPipelineTest.class})
+@SuiteClasses({JenkinsTest.class, JenkinsWithoutMaven.class})
 public class JenkinsTestSuite {
 
   @ClassRule
