d3d3b8d29c838e6a4894f61b57bf98c2af2ecf01
Julien HENRY <julien.henry@sonarsource.com>
2018-01-08 14:49:38 +0100

    Use Maven version of travis-utils

--- travis.sh
+++ travis.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v42 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v43 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
+. ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
 
